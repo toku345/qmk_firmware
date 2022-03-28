09e87e138f434498020ee9e48ff27eb70baa5a8c
Fumitaka Tokumitsu <fumitaka.tokumitsu@classi.jp>
2022-03-28 15:45:38 +0900

    Add cursor keys

--- keyboards/ergodox_ez/keymaps/toku345/keymap.c
+++ keyboards/ergodox_ez/keymaps/toku345/keymap.c
@@ -24,11 +24,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  | LCtrl |      |      | LAlt | LGUI |                                       |   B  |  [/{ |  ]/} |      | ~L1  |
  *  `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                        | Left |Right |       | Left | Right  |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
+ *                                 |      |      |  Up  |       |  Up  |        |      |
  *                                 |LShift|Back  |------|       |------|  Enter |RShift|
- *                                 |/Space|space | End  |       | PgDn |        |/Space|
+ *                                 |/Space|space | Down |       | Down |        |/Space|
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -40,18 +40,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_A,   KC_S,   KC_D,     KC_F,     KC_G,
         KC_LSFT,  KC_Z,   KC_X,   KC_C,     KC_V,     KC_B,  KC_RBRC,
         KC_LCTL,  KC_NO,  KC_NO,  KC_LALT,  KC_LGUI,
-                                                  ALT_T(KC_APP),  KC_LGUI,
-                                                                  KC_HOME,
-                                       LSFT_T(KC_SPC),  KC_BSPC,  KC_END,
+                                                        KC_LEFT,  KC_RGHT,
+                                                                  KC_UP  ,
+                                       LSFT_T(KC_SPC),  KC_BSPC,  KC_DOWN,
         // right hand
              KC_7,     KC_8,   KC_9,   KC_0,     KC_MINS,  KC_EQL,   KC_GRV,
              KC_LBRC,  KC_Y,   KC_U,   KC_I,     KC_O,     KC_P,     KC_BSLS,
                        KC_H,   KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
              KC_RBRC,  KC_N,   KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
                                KC_B,   KC_LBRC,  KC_RBRC,  KC_NO,   TT(SYMB),
-             KC_LALT,  CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN,  KC_ENT,  RSFT_T(KC_SPC)
+             KC_LEFT,  KC_RGHT,
+             KC_UP,
+             KC_DOWN,  KC_ENT,  RSFT_T(KC_SPC)
     ),
 /* Keymap 1: Symbol Layer
  *
