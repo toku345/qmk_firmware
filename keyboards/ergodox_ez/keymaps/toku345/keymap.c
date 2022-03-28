364c9061bcc588479bb05481e1ce11fbb30ba195
Fumitaka Tokumitsu <fumitaka.tokumitsu@classi.jp>
2022-03-28 15:39:41 +0900

    🔧 Fix keymap.c to enable compilation

--- keyboards/ergodox_ez/keymaps/toku345/keymap.c
+++ keyboards/ergodox_ez/keymaps/toku345/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_LBRC,  KC_Y,   KC_U,   KC_I,     KC_O,     KC_P,     KC_BSLS,
                        KC_H,   KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
              KC_RBRC,  KC_N,   KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-                               KC_B,   KC_LBRC,  KC_RBRC,  KC_NO,    KC_FN1,
+                               KC_B,   KC_LBRC,  KC_RBRC,  KC_NO,   TT(SYMB),
              KC_LALT,  CTL_T(KC_ESC),
              KC_PGUP,
              KC_PGDN,  KC_ENT,  RSFT_T(KC_SPC)
@@ -136,30 +136,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_WBAK
 ),
-};
-
-const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
-};
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-  switch(id) {
-    case 0:
-      if (record->event.pressed) {
-        register_code(KC_RSFT);
-      } else {
-        unregister_code(KC_RSFT);
-      }
-      break;
-  }
-  return MACRO_NONE;
-};
-
-// Runs just one time when the keyboard initializes.
-void matrix_init_user(void) {
-
 };
 
 // Runs constantly in the background, in a loop.
