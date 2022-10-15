81e716869e507dd5ddf6d9d07fc2c95e8db58310
Fumitaka Tokumitsu <toku345@gmail.com>
2022-10-15 16:18:28 +0900

    Change Keymap 1: Symbol Layer -> For Windows Layer

--- keyboards/ergodox_ez/keymaps/toku345/keymap.c
+++ keyboards/ergodox_ez/keymaps/toku345/keymap.c
@@ -10,7 +10,7 @@
 #define MDIA 2 // media keys
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap 0: Basic layer
+/* Keymap 0: For macOS
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | ESC    |  1/! |  2/@ |  3/# |  4/$ |  5/% |  6/^ |           |  7/& | 8/ * |  9/( |  0/) |  -/_ |  =/+ |  `/~   |
@@ -53,48 +53,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_UP,
              KC_DOWN,  KC_ENT,  RSFT_T(KC_SPC)
     ),
-/* Keymap 1: Symbol Layer
+/* Keymap 1: For Windows
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           | Reset|  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+ * | ESC    |  1/! |  2/@ |  3/# |  4/$ |  5/% |  6/^ |           |  7/& | 8/ * |  9/( |  0/) |  -/_ |  =/+ |  `/~   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [/{ |           |  [/{ |   Y  |   U  |   I  |   O  |   P  |   \/|  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
+ * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;/: |  '/"   |
+ * |--------+------+------+------+------+------|  ]/} |           |  ]/} |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,/< |  ./> |  //? | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |    . |   0  |   =  |      |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
+ *  | LCtrl |      |      | LGUI | LAlt |                                       |   B  |  [/{ |  ]/} |      |      |
+ *  `-----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,---------------.
+ *                                        | Left |Right |       | Left | Right  |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      |  Up  |       |  Up  |        |      |
+ *                                 |LShift|Back  |------|       |------|  Enter |RShift|
+ *                                 |/Space|space | Down |       | Down |        |/Space|
+ *                                 `--------------------'       `----------------------'
  */
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
-       // left hand
-       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-                                       KC_TRNS,KC_TRNS,
-                                               KC_TRNS,
-                               KC_TRNS,KC_TRNS,KC_TRNS,
-       // right hand
-       RESET,   KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
-),
+        // left hand
+        KC_ESC,   KC_1,   KC_2,   KC_3,     KC_4,     KC_5,  KC_6,
+        KC_TAB,   KC_Q,   KC_W,   KC_E,     KC_R,     KC_T,  KC_LBRC,
+        KC_LCTL,  KC_A,   KC_S,   KC_D,     KC_F,     KC_G,
+        KC_LSFT,  KC_Z,   KC_X,   KC_C,     KC_V,     KC_B,  KC_RBRC,
+        KC_LCTL,  KC_NO,  KC_NO,  KC_LGUI,  KC_LALT,
+                                                        KC_LEFT,  KC_RGHT,
+                                                                  KC_UP  ,
+                                       LSFT_T(KC_SPC),  KC_BSPC,  KC_DOWN,
+        // right hand
+             KC_7,     KC_8,   KC_9,   KC_0,     KC_MINS,  KC_EQL,   KC_GRV,
+             KC_LBRC,  KC_Y,   KC_U,   KC_I,     KC_O,     KC_P,     KC_BSLS,
+                       KC_H,   KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+             KC_RBRC,  KC_N,   KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+                               KC_B,   KC_LBRC,  KC_RBRC,  KC_NO,    KC_TRNS,
+             KC_LEFT,  KC_RGHT,
+             KC_UP,
+             KC_DOWN,  KC_ENT,  RSFT_T(KC_SPC)
+    ),
 /* Keymap 2: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
